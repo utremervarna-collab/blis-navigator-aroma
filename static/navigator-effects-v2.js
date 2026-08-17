21a516e54d6c645209578ebc5975b7778b45a53f
utremervarna-collab <utremervarna@gmail.com>
2026-08-17 11:24:23 +0300

    Bump daily overview runtime to v5

--- static/navigator-effects-v2.js
+++ static/navigator-effects-v2.js
@@ -16,7 +16,7 @@
 (function(){
   function loadScript(src,next){var s=document.createElement('script');s.src=src;s.onload=function(){if(next)next()};document.head.appendChild(s)}
   loadScript('/navigator-period-filter.js?v=20260817-4',function(){
-    loadScript('/navigator-period-overview.js?v=20260817-4',function(){
+    loadScript('/navigator-period-overview.js?v=20260817-5',function(){
       loadScript('/navigator-period-runtime.js?v=20260817-3',function(){
         loadScript('/navigator-client-runtime-fix.js?v=20260817-1');
       });
