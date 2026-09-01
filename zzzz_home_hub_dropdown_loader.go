cd3e8d994330bbd3d6211a61e9670226adc10016
utremervarna-collab <utremervarna@gmail.com>
2026-09-01 19:23:11 +0300

    Bump Intelligence Hub menu cache version

--- zzzz_home_hub_dropdown_loader.go
+++ zzzz_home_hub_dropdown_loader.go
@@ -3,9 +3,9 @@ package main
 import "strings"
 
 func init() {
-	const marker = `/home-hub-dropdown.js?v=20260901-click-menu2`
+	const marker = `/home-hub-dropdown.js?v=20260901-click-menu3`
 	if strings.Contains(indexHTML, marker) {
 		return
 	}
-	indexHTML = strings.Replace(indexHTML, `</body>`, `<script src="/home-hub-dropdown.js?v=20260901-click-menu2"></script></body>`, 1)
+	indexHTML = strings.Replace(indexHTML, `</body>`, `<script src="/home-hub-dropdown.js?v=20260901-click-menu3"></script></body>`, 1)
 }
