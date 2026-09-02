c5cea805f89d427e8196b3f955a52afc70522467
utremervarna-collab <utremervarna@gmail.com>
2026-09-02 11:12:27 +0300

    Load KUB shell fix before language layer

---
+++ kub_crisis_shell_loader.go
@@ -0,0 +1,10 @@
+package main
+
+import "bytes"
+
+func init() {
+	const kubShellScript = `<script src="/kub-crisis-shell-fix-v1.js?v=20260902-shell1"></script>`
+	if !bytes.Contains(blisI18NScripts, []byte("kub-crisis-shell-fix-v1.js")) {
+		blisI18NScripts = append([]byte(kubShellScript), blisI18NScripts...)
+	}
+}
