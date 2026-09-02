d69437bfb01043f813b364e3a76f609de81e2912
utremervarna-collab <utremervarna@gmail.com>
2026-09-02 17:17:28 +0300

    Retire duplicate KUB live signal renderer

--- kub_live_signals_loader.go
+++ kub_live_signals_loader.go
@@ -1,10 +1,4 @@
 package main
 
-import "bytes"
-
-func init() {
-	const kubLiveSignalsScript = `<script defer src="/kub-live-signals-v1.js?v=20260902-live1"></script>`
-	if !bytes.Contains(blisI18NScripts, []byte("kub-live-signals-v1.js")) {
-		blisI18NScripts = append([]byte(kubLiveSignalsScript), blisI18NScripts...)
-	}
-}
+// Legacy KUB live-signals v1 loader retired.
+// Feed v3 is the only client-side Monitoring renderer.
