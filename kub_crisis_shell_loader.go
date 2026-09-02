1cebd3b1b264e022b27bf8fae683c9371186ee62
utremervarna-collab <utremervarna@gmail.com>
2026-09-02 12:13:14 +0300

    Load white 3D KUB pressure-map view

--- kub_crisis_shell_loader.go
+++ kub_crisis_shell_loader.go
@@ -3,6 +3,11 @@ package main
 import "bytes"
 
 func init() {
+	const kubAttackMapWhite3dScript = `<script defer src="/kub-attack-map-white3d-v1.js?v=20260902-white3d1"></script>`
+	if !bytes.Contains(blisI18NScripts, []byte("kub-attack-map-white3d-v1.js")) {
+		blisI18NScripts = append([]byte(kubAttackMapWhite3dScript), blisI18NScripts...)
+	}
+
 	const kubAttackMapLiveScript = `<script defer src="/kub-attack-map-live-v1.js?v=20260902-live2"></script>`
 	if !bytes.Contains(blisI18NScripts, []byte("kub-attack-map-live-v1.js")) {
 		blisI18NScripts = append([]byte(kubAttackMapLiveScript), blisI18NScripts...)
