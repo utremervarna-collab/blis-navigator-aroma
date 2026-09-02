9a1aeda5fc4695a090cd15f989cf2cd0b901b5c7
utremervarna-collab <utremervarna@gmail.com>
2026-09-02 17:17:17 +0300

    Retire duplicate KUB monitoring health badge

--- kub_monitoring_health_loader.go
+++ kub_monitoring_health_loader.go
@@ -1,10 +1,4 @@
 package main
 
-import "bytes"
-
-func init() {
-	const tag = `<script defer src="/kub-monitoring-health-v1.js?v=20260902-health1"></script>`
-	if !bytes.Contains(blisI18NScripts, []byte("kub-monitoring-health-v1.js")) {
-		blisI18NScripts = append([]byte(tag), blisI18NScripts...)
-	}
-}
+// Duplicate KUB monitoring health badge retired.
+// Feed v3 exposes the single client-visible monitoring status.
