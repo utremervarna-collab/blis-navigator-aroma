dd07dc572f7b83ab7c37a651e3356a0aca14bc8b
utremervarna-collab <utremervarna@gmail.com>
2026-09-02 11:52:12 +0300

    Load KUB pressure network module

--- kub_crisis_shell_loader.go
+++ kub_crisis_shell_loader.go
@@ -3,7 +3,12 @@ package main
 import "bytes"
 
 func init() {
-	const kubShellScript = `<script src="/kub-crisis-shell-fix-v1.js?v=20260902-shell4"></script>`
+	const kubAttackMapScript = `<script src="/kub-attack-map-v1.js?v=20260902-attack1"></script>`
+	if !bytes.Contains(blisI18NScripts, []byte("kub-attack-map-v1.js")) {
+		blisI18NScripts = append([]byte(kubAttackMapScript), blisI18NScripts...)
+	}
+
+	const kubShellScript = `<script src="/kub-crisis-shell-fix-v1.js?v=20260902-shell5"></script>`
 	if !bytes.Contains(blisI18NScripts, []byte("kub-crisis-shell-fix-v1.js")) {
 		blisI18NScripts = append([]byte(kubShellScript), blisI18NScripts...)
 	}
