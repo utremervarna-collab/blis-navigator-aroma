c9a548375d6357f13ecd79f98a3c301bce2f98d2
utremervarna-collab <utremervarna@gmail.com>
2026-09-02 11:55:32 +0300

    Enable live KUB pressure map overlay

--- kub_crisis_shell_loader.go
+++ kub_crisis_shell_loader.go
@@ -3,12 +3,17 @@ package main
 import "bytes"
 
 func init() {
+	const kubAttackMapLiveScript = `<script defer src="/kub-attack-map-live-v1.js?v=20260902-live1"></script>`
+	if !bytes.Contains(blisI18NScripts, []byte("kub-attack-map-live-v1.js")) {
+		blisI18NScripts = append([]byte(kubAttackMapLiveScript), blisI18NScripts...)
+	}
+
 	const kubAttackMapScript = `<script src="/kub-attack-map-v1.js?v=20260902-attack1"></script>`
 	if !bytes.Contains(blisI18NScripts, []byte("kub-attack-map-v1.js")) {
 		blisI18NScripts = append([]byte(kubAttackMapScript), blisI18NScripts...)
 	}
 
-	const kubShellScript = `<script src="/kub-crisis-shell-fix-v1.js?v=20260902-shell5"></script>`
+	const kubShellScript = `<script src="/kub-crisis-shell-fix-v1.js?v=20260902-shell6"></script>`
 	if !bytes.Contains(blisI18NScripts, []byte("kub-crisis-shell-fix-v1.js")) {
 		blisI18NScripts = append([]byte(kubShellScript), blisI18NScripts...)
 	}
