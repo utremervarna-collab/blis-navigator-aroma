61bb1ed755c42fc1073e0b563c67f60061dad7c7
utremervarna-collab <utremervarna@gmail.com>
2026-09-02 11:21:05 +0300

    Bump KUB shell fix cache version

--- kub_crisis_shell_loader.go
+++ kub_crisis_shell_loader.go
@@ -3,7 +3,7 @@ package main
 import "bytes"
 
 func init() {
-	const kubShellScript = `<script src="/kub-crisis-shell-fix-v1.js?v=20260902-shell1"></script>`
+	const kubShellScript = `<script src="/kub-crisis-shell-fix-v1.js?v=20260902-shell2"></script>`
 	if !bytes.Contains(blisI18NScripts, []byte("kub-crisis-shell-fix-v1.js")) {
 		blisI18NScripts = append([]byte(kubShellScript), blisI18NScripts...)
 	}
