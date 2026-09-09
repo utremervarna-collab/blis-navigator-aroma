bba7eb210a43c03a906753d68baf917d34635713
utremervarna-collab <utremervarna@gmail.com>
2026-09-09 15:58:14 +0300

    Add BLIS Navigator service worker

---
+++ static/blis-sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'blis-navigator-pwa-v1';
+const APP_SHELL = [
+  '/dashboard.html',
+  '/manifest.webmanifest',
+  '/blis-app-icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).catch(() => undefined));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).catch(() => caches.match('/dashboard.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(req, copy)).catch(() => undefined);
+        return res;
+      })
+      .catch(() => caches.match(req))
+  );
+});
