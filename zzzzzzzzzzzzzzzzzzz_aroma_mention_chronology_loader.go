13aaf9c8c0bc07522f783355be17b7bab274375e
utremervarna-collab <utremervarna@gmail.com>
2026-09-14 14:01:12 +0300

    Load Aroma chronology and active competition strip

--- zzzzzzzzzzzzzzzzzzz_aroma_mention_chronology_loader.go
+++ zzzzzzzzzzzzzzzzzzz_aroma_mention_chronology_loader.go
@@ -3,8 +3,12 @@ package main
 import "bytes"
 
 func init() {
-	const script = `<script defer src="/aroma-mention-chronology-v1.js?v=20260914-1"></script>`
+	const chronologyScript = `<script defer src="/aroma-mention-chronology-v1.js?v=20260914-2"></script>`
+	const activationScript = `<script defer src="/aroma-competition-activation-v1.js?v=20260914-1"></script>`
 	if !bytes.Contains(blisI18NScripts, []byte("aroma-mention-chronology-v1.js")) {
-		blisI18NScripts = append(blisI18NScripts, []byte(script)...)
+		blisI18NScripts = append(blisI18NScripts, []byte(chronologyScript)...)
+	}
+	if !bytes.Contains(blisI18NScripts, []byte("aroma-competition-activation-v1.js")) {
+		blisI18NScripts = append(blisI18NScripts, []byte(activationScript)...)
 	}
 }
