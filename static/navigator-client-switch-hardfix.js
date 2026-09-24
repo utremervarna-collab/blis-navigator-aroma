0cc9e7ed4e160b1240a2e9246b4bf9a2afc52ffe
utremervarna-collab <utremervarna@gmail.com>
2026-09-24 17:28:32 +0300

    Add Delta Planet to navigator-client-switch-hardfix.js client registry

--- static/navigator-client-switch-hardfix.js
+++ static/navigator-client-switch-hardfix.js
@@ -3,7 +3,7 @@
   'use strict';
   if(window.__BLISClientSwitchHardfix)return;
   window.__BLISClientSwitchHardfix=true;
-  const allowed=new Set(['aroma','bolyarka','astor-garden','varna-towers','mollox','wirello','everbet','black-sea-center']);
+  const allowed=new Set(['aroma','bolyarka','astor-garden','varna-towers','delta-planet','mollox','wirello','everbet','black-sea-center']);
 
   async function switchInPlace(key){
     if(!allowed.has(key))return;
