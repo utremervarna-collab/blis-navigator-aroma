717a26e838997560c1257f7b9944588a7f258734
utremervarna-collab <utremervarna@gmail.com>
2026-09-25 09:19:37 +0300

    Bump exact release marker after runtime fixes

--- static/migration-build.js
+++ static/migration-build.js
@@ -3,4 +3,4 @@
 window.__BLIS_MIGRATION_BUILD__='code-run-migration-2026-09-09-final-v2';
 window.__BLIS_MIGRATION_COMPAT_BUILD__='code-run-migration-2026-09-09-v1';
 
-window.__BLIS_RELEASE__='bsc-home-2026-09-25-v1';
+window.__BLIS_RELEASE__='bsc-home-2026-09-25-v2';
