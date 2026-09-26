331f1d6cba42b5b416ff45c277b983557d7661fc
utremervarna-collab <utremervarna@gmail.com>
2026-09-26 18:39:59 +0300

    Mark teaser search funnel release

--- static/migration-build.js
+++ static/migration-build.js
@@ -3,4 +3,4 @@
 window.__BLIS_MIGRATION_BUILD__='code-run-migration-2026-09-09-final-v2';
 window.__BLIS_MIGRATION_COMPAT_BUILD__='code-run-migration-2026-09-09-v1';
 
-window.__BLIS_RELEASE__='home-intelligence-search-2026-09-26-v2';
+window.__BLIS_RELEASE__='home-intelligence-search-2026-09-26-v3';
